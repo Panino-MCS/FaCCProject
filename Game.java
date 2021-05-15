760d70b79960c5656411d3f1a07dc8ae6d0eeaa8
Panino-MCS <andrea.traldi@studenti.unitn.it>
2021-05-15 10:14:59 +0200

    Commit 3

--- Game.java
+++ Game.java
@@ -1,9 +1,64 @@
+import java.util.*;
+
 public class Game {
   public static void main(String[] args){
     
-    Player p = new Player(100, 100);
-    System.out.println("Player hp: " + p.getHp());
-    System.out.println("Player mp: " + p.getMp());
+    Player p = new Player("Bomber", 100, 100, 15, 10, 0, 0);
 
+    Scanner scanchoice = new Scanner(System.in);
+    int choiceentry = 0;
+    boolean stay = true;
+
+    while(stay){
+    
+      System.out.println("Menu - Press a number to continue: ");
+      System.out.println("\"1\" -> Start e new fight");
+      System.out.println("\"2\" -> Enter the shop");
+      System.out.println("\"3\" -> Player' status");
+      System.out.println("\"4\" -> Exit");
+      if(scanchoice.hasNextInt()){
+        choiceentry = scanchoice.nextInt();
+          if(choiceentry < 1 || choiceentry > 3){
+            System.out.println("Exiting the game...");
+            stay = false;
+          }
+        menuSelection(choiceentry, p);
+      }
+      else {
+        stay = false;
+      }
+    }
+  }
+
+  private static void menuSelection(int selection, Player p){
+    switch(selection){
+      case 1:
+        //Fight
+        System.out.println("Figth!");
+        break;
+      case 2:
+        //Merchant
+        System.out.println("Merchant!");
+        break;
+      case 3:
+        //Status
+        System.out.println("---Status---");
+        System.out.println(p.toString());
+        pressEnterKeyToContinue();
+        break;
+      default:
+        break;
+    }
+  }
+
+  private static void pressEnterKeyToContinue()
+  { 
+         System.out.println("Press Enter key to continue...");
+         try
+         {
+             System.in.read();
+         }  
+         catch(Exception e)
+         {}  
   }
 }
--- Player.java
+++ Player.java
@@ -1,11 +1,37 @@
 public class Player {
     
+    private String name;
     private int hp;
     private int mp;
+    private int baseAtk;
+    private int baseMag;
+    private int hPotions;
+    private int mPotions;
     
-    public Player(int hp, int mp){
+    public Player(String name, int hp, int mp, int baseAtk, int baseMag, int hPotions, int mPotions){
+        this.name = name;
         this.hp = hp;
         this.mp = mp;
+        this.baseAtk = baseAtk;
+        this.baseMag = baseMag;
+        this.hPotions = hPotions;
+        this.mPotions = mPotions;
+    }
+
+    public String toString(){
+        String result = "Player : \"" + this.name + "\"\n";
+        result = result + "Hp - " + this.hp + "\n";
+        result = result + "Mp - " + this.mp + "\n";
+        result = result + "Atk - " + this.baseAtk + "\n";
+        result = result + "Mag - " + this.baseMag + "\n";
+        result = result + "Health potions held - " + this.hPotions + "\n";
+        result = result + "Mana potions held - " + this.mPotions + "\n";
+
+        return result;
+    }
+
+    public String getName(){
+        return this.name;
     }
 
     public int getHp(){
@@ -16,12 +42,48 @@ public class Player {
         return this.mp;
     }
 
+    public int getBaseAtk(){
+        return this.baseAtk;
+    }
+
+    public int getBaseMag(){
+        return this.baseMag;
+    }
+
+    public int getHPotions(){
+        return this.hPotions;
+    }
+
+    public int getMPotions(){
+        return this.mPotions;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
     public void setHp(int newHp){
         this.hp = newHp;
     }
 
     public void setMp(int newMp){
         this.mp = newMp;
+    }
+
+    public void setBaseAtk(int baseAtk){
+        this.baseAtk = baseAtk;
+    }
+
+    public void setBaseMag(int baseMag){
+        this.baseMag = baseMag;
+    }
+
+    public void setHPotions(int hPotions){
+        this.hPotions = hPotions;
+    }
+
+    public void setMPotions(int mPotions){
+        this.mPotions = mPotions;
     }
 
   }
